2e5c2aab6c76834fdac3aebec2eb357c39026224
samuel <sbacon803@gmail.com>
2022-08-27 14:02:01 -0600

    add index action

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
+<script>
+import axios from 'axios';
+
+  export default {
+    data: function () {
+      return {
+        message: "Welcome to Vue.js!",
+        places: []
+      };
+    },
+    created: function () {
+      this.indexPlaces();
+    },
+    methods: {
+      indexPlaces: function () {
+        console.log('places index..')
+        axios.get("/places").then(response => {
+          console.log(response.data); 
+          this.places = response.data
+        })
+      }
+    },
+  };
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+   <!-- <h1>{{ places }}</h1> -->
+   <div v-for="place in places">
+     <p>{{ place.name }}</p>
+     <p>{{ place.address }}</p>
+     <hr />
   </div>
+</div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<style></style>

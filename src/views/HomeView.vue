bb5b1fa5eec89962394cfbf996b839cadb075a40
samuel <sbacon803@gmail.com>
2022-08-27 18:59:45 -0600

    add post create

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,8 @@ import axios from 'axios';
     data: function () {
       return {
         message: "Welcome to Vue.js!",
-        places: []
+        places: [],
+        newPlaceParams: {}, 
       };
     },
     created: function () {
@@ -18,19 +19,43 @@ import axios from 'axios';
           console.log(response.data); 
           this.places = response.data
         })
+      },
+      createPlace: function () {
+        axios.post("/places", this.newPlaceParams).then((response) => {
+          console.log("places create", response);
+          this.places.push(response.data);
+          this.newPlaceParams = {};
+        })
+        .catch((error) => {
+          console.log("places create error", error.response);
+        });
       }
     },
   };
 </script>
 
+
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+      <h1>New Place</h1>
+      <div>
+      Name:
+      <input type="text" v-model="newPlaceParams.name" />
+      Address:
+      <input type="text" v-model="newPlaceParams.address" />
+      image_url:
+      <input type="text" v-model="newPlaceParams.url" />
+     <p> <button v-on:click="createPlace()">Create Place</button></p>
+      </div>
    <!-- <h1>{{ places }}</h1> -->
+   <h1>All Places</h1>
    <div v-for="place in places">
      <p>{{ place.name }}</p>
      <p>{{ place.address }}</p>
-     <hr />
+     <p>{{ place.image_url }}</p>
+     <br/>
+     <!-- <button v-on:click="createPlace">Create New Place</button> -->
   </div>
 </div>
 </template>

29611b42c1d4e4f1d1088f34279cad16ea49d30f
samuel <sbacon803@gmail.com>
2022-08-31 22:37:56 -0600

    add show

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,9 +4,10 @@ import axios from 'axios';
   export default {
     data: function () {
       return {
-        message: "Welcome to Vue.js!",
+        message: "Welcome to Places!!",
         places: [],
         newPlaceParams: {}, 
+        currentPlace: {},
       };
     },
     created: function () {
@@ -29,6 +30,10 @@ import axios from 'axios';
         .catch((error) => {
           console.log("places create error", error.response);
         });
+      },
+      showPlace: function (place) {
+        this.currentPlace = place;
+        document.querySelector("place-details").showModal();
       }
     },
   };
@@ -37,26 +42,33 @@ import axios from 'axios';
 
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
-      <h1>New Place</h1>
+      <h3>New Place:</h3>
       <div>
       Name:
       <input type="text" v-model="newPlaceParams.name" />
       Address:
       <input type="text" v-model="newPlaceParams.address" />
       image_url:
-      <input type="text" v-model="newPlaceParams.url" />
+      <input type="img" v-model="newPlaceParams.image_url" />
      <p> <button v-on:click="createPlace()">Create Place</button></p>
       </div>
    <!-- <h1>{{ places }}</h1> -->
-   <h1>All Places</h1>
-   <div v-for="place in places">
+   <h3>~All Places~</h3>
+   <div v-for="place in places" v-bind:key="place.id">
      <p>{{ place.name }}</p>
      <p>{{ place.address }}</p>
-     <p>{{ place.image_url }}</p>
-     <br/>
-     <!-- <button v-on:click="createPlace">Create New Place</button> -->
+     <img v-bind:src="place.image_url" v-bind:alt="place.name" />
+     <button v-on:click="showPlace(place)">More info</button>
   </div>
+  <dialog id="place-details">
+   <form method="dialog">
+     <h1>Place info</h1>
+     <p>Name: {{ currentPlace.name }}</p>
+     <p>address: {{ currentPlace.address }}</p>
+     <p>image_url: {{ currentPlace.image_url }}</p>
+     <button>Close</button>
+    </form>
+  </dialog>
 </div>
 </template>
 
